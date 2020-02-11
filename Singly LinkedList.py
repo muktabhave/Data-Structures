d43d8ab72162c5147210ad408025d6a301d2d60a
Mukta Girkar <muktagirkar@gmail.com>
2020-02-10 16:38:10 -0800

    Update Singly LinkedList.py

--- Singly LinkedList.py
+++ Singly LinkedList.py
@@ -7,6 +7,7 @@ class LinkedList:
     def __init__(self):
         self.head= None
     
+    #push element at end
     def pushll(self,ele):
         #if LL is empty
         if(self.head== None):
@@ -15,11 +16,10 @@ class LinkedList:
         else:
             tmp= self.head
             
-            while(tmp!=None):
-                prev= tmp
+            while(tmp.next!=None):
                 tmp=tmp.next
             
-            prev.next=Node(ele)
+            tmp.next=Node(ele)
     
     def popll(self,ele):
         #if list is empty
@@ -33,14 +33,13 @@ class LinkedList:
                 self.head=tmp.next
             else:
                 
-                while(tmp.data!=ele and tmp!=None):
-                    prev=tmp
+                while(tmp.next.data!=ele and tmp!=None):
                     tmp=tmp.next
             
                 if (tmp==None):
                     print("Node to be deleted not found")
                 else:
-                    prev.next=tmp.next
+                    tmp.next=tmp.next.next
     
     def printll(self):
         
@@ -62,10 +61,10 @@ if (__name__=='__main__'):
     ll.printll()
     
     ll.popll(30)
-    
+    ll.printll()   
     ll.popll(10)
     ll.printll()
     ll.popll(20)
-    
+    ll.printll()
     ll.popll(10)
     
